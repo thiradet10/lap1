34a9e1f6c453d849248dcf42dda9120ef7b52a9e
thiradet10 <thiradet.pr@kkumail.com>
2024-11-27 11:14:45 +0700

    add

---
+++ iap.1/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+string name = "thiradet Prasansuk";
+string student id = "673450195-4";
+// string is datatype -> ชนิดของข้อมูล
+// name is variable
+// "..." is value of variable
+Console.WriteLine("Hello, World!");
+Console.WriteLine("My name is thiradet prasansuk");
+Console.WriteLine("My student id : 673450195-4");
